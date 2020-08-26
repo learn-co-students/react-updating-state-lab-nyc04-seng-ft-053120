cc974738b5d99171d7e502522ca546f964395a10
guillenjs <jorgeguilln@gmail.com>
2020-08-26 17:50:07 -0400

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,33 @@
 // Code DigitalClicker Component Here
+import React from 'react';
+
+
+class DigitalClicker extends React.Component {
+
+    // constructor() {
+    //     super()
+    //     this.state = {
+    //         timeClicked: 0
+    //     }
+    // }
+
+state = {
+    timesClicked: 0
+};
+
+handleCount = () => {
+    // console.log(this.state.timeClicked)
+    this.setState(previousState => {
+        // console.log(previousState.timeClicked += 1)
+        return {timesClicked: previousState.timesClicked += 1}
+    })
+}
+
+    render() {
+        return(
+            <button onClick={this.handleCount}>{this.state.timesClicked}</button>
+        )
+    }
+}
+
+export default DigitalClicker
